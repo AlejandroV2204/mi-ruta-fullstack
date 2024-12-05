e5618fd93b7d13ad6b88139efea0b1dbb016c6b5
AlejandroV2204 <91919753+AlejandroV2204@users.noreply.github.com>
2024-12-05 00:07:22 -0500

    puerto condicional para el back, cambio de puerto de vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001 // Cambia el puerto aquí
+    port: 5173 
   }
 })
--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
-export const PORT = 5000
+const PORT = process.env.PORT || 3000;
 export const TOKEN_SECRET = 'token123' 
